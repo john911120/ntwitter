df9e6d12f31c071622a9cb94f1dd4a10e0fd51b0
john911120 <53751665+john911120@users.noreply.github.com>
2022-03-12 21:19:59 +0900

    git push origin master

--- src/components/App.js
+++ src/components/App.js
@@ -6,10 +6,12 @@ function App() {
 
   const [init, setInit] = useState(false)
   const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [userObj, setUserObj] = useState(null)
   useEffect(() => {
     authService.onAuthStateChanged((user) => {
       if(user) {
         setIsLoggedIn(user);
+        setUserObj(user);
       } else {
         setIsLoggedIn(false);
       }
@@ -19,7 +21,11 @@ function App() {
 
   return (
     <>   
-    { init ? <AppRouter isLoggedIn={isLoggedIn}/> : "Initializing..." } 
+    { init ?(
+       <AppRouter isLoggedIn={isLoggedIn} userObj={userObj}/>
+      ):( 
+      "Initializing..." 
+      )} 
     <footer>&copy; {new Date().getFullYear()} Ntwitter</footer>
     </>
    );
