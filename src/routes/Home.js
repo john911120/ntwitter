6fef11fac3073eacc3b941a81385d3d6aadb4fc1
john911120 <53751665+john911120@users.noreply.github.com>
2022-03-19 10:46:27 +0900

    git push origin master

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import { dbService } from 'fbase';
+import Nweet from "components/Nweet";
 
 const Home = ({userObj}) => {
     const [nweet, setNweet] = useState("");
@@ -56,9 +57,11 @@ const Home = ({userObj}) => {
         </form>
         <div>
             {nweets.map((nweet) => (
-                <div key={nweet.id}>
-                    <h4>{nweet.text}</h4>
-                </div>
+                <Nweet 
+                key={nweet.id} 
+                nweetObj={nweet}
+                isOwner={nweet.creatorId === userObj.uid}
+                />
             ))}
         </div>
         </>
