aec6ff85ab01ef4b1a5629b166c53bbdcf596be2
john911120 <wtt54@naver.com>
2022-03-20 11:19:37 +0900

    git push origin master

--- src/components/Router.js
+++ src/components/Router.js
@@ -5,19 +5,19 @@ import Home from "routes/Home";
 import Profile from "routes/Profile";
 import Navigation from "./Navigation";
 
-const AppRouter = ({isLoggedIn, userObj}) => {
+const AppRouter = ({isLoggedIn, userObj, refreshUser }) => {
     return (
         <Router>
-            {isLoggedIn && <Navigation/>}
+            {isLoggedIn && <Navigation userObj={userObj}/>}
             <Switch>
                 {isLoggedIn ? (
                 <>
                 <Route exact path="/">
                     <Home userObj={userObj}/>
                 </Route>
-                <Route exact path="/profile">
-                    <Profile/>
-                </Route>
+                    <Route exact path="/profile">
+                        <Profile refreshUser={refreshUser} userObj={userObj}/>
+                    </Route>
                 </>
                 ) : (
                     <Route exact path="/">
--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,16 +1,49 @@
-import { authService } from "fbase";
+import { authService, dbService } from "fbase";
+import { useState, useEffect } from "react";
 import { useHistory } from "react-router-dom";
 
-const Profile = () => {
+const Profile = ({userObj, refreshUser }) => {
     const history = useHistory();
+    const [newDisplayName, setNewDisplayName] = useState(userObj.displayName)
 
     const onLogOutClick = () => {
         authService.signOut();
         history.push("/");
     };
 
+    const onChange = (event) => {
+        const {
+            target : {value},
+        } = event
+        setNewDisplayName(value)
+    }
+
+/*
+    const getMyNweets = async () => {
+        const nweets = await dbService
+        .collection("nweets")
+        .where("creatorId", "==", userObj.uid)
+        .orderBy("createdAt","asc")
+        .get()
+        console.log(nweets.docs.map((doc) => doc.data()))
+    }
+
+    useEffect(() => {getMyNweets()}, [])
+*/
+    const onSubmit = async (event) => {
+        event.preventDefault()
+        if(userObj.displayName !== newDisplayName){
+            await userObj.updateProfile({displayName : newDisplayName})
+            refreshUser()
+        }
+    }
+
     return (
         <>
+            <form onSubmit={onSubmit}>
+                <input onChange={onChange}  type="text" placeholder="Display Name" value={newDisplayName}></input>
+                <input type="submit" value="Update Profile"></input>
+            </form>
             <button onClick={onLogOutClick}>LogOut</button>
         </>
     );
